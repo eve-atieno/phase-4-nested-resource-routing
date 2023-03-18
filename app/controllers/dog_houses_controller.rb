7b62659155f9f3e5eb6599df335ecf099e5e4073
eve-atieno <atienoevelyne53@gmail.com>
2023-03-18 22:09:18 +0300

    completed assignment

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -2,11 +2,16 @@ class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
-    dog_house = DogHouse.find(params[:id])
-    render json: dog_house
+    dog_house = DogHouse.find(params[:dog_house_id])
+    reviews = dog_house.reviews
+    render json: reviews, include: :dog_house
   end
 
-  private
+    def review
+    review = Review.find(params[:id])
+    render json: review, include: :dog_house
+  end
+  private# config/routes.rb
 
   def render_not_found_response
     render json: { error: "Dog house not found" }, status: :not_found
